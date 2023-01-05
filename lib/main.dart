7250bc4519c2d2209884163113a7511b75baba9c
sailu <vermasailesh271@gmail.com>
2023-01-05 14:26:13 +0530

    intial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:useless_button/screen/ad_mob_page.dart';
 import 'package:useless_button/screen/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const AdMobPage(),
     );
   }
 }
---
+++ lib/screen/ad_mob_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import "package:google_mobile_ads/google_mobile_ads.dart";
+
+class AdMobPage extends StatefulWidget {
+  const AdMobPage({Key? key}) : super(key: key);
+
+  @override
+  State<AdMobPage> createState() => _AdMobPageState();
+}
+
+class _AdMobPageState extends State<AdMobPage> {
+  @override
+  void initState() {
+   
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
